0f43aa29bfa405dc04f92b96f6bb293020c290d2
Mateus Capaldo Martins <mateus.c.martins@outlook.com>
2022-05-02 17:47:54 -0300

    Inicio de uma tela de login

---
+++ TelaLogin.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TelaLogin {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame();
+        frame.setSize(600,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JPanel painel = new JPanel();
+
+
+    }
+}
